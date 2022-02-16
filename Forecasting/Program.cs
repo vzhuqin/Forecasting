08e3a04168f38bd8a8486b32963a45a6a2048529
vzhuqin <vzhuqin@vzhuqin1228>
2022-02-16 01:45:03 -0800

    Add project files.

---
+++ Forecasting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Forecasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
